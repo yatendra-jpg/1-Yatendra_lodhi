2323a80c414537bf23184e14ffa85197c4f1bf39
yatendra-jpg <yatendra@yatendrabulkmailng.info>
2025-12-09 21:55:41 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,48 +1,31 @@
-logoutBtn.addEventListener("dblclick",()=>{
-  fetch("/logout",{method:"POST"})
+logoutBtn.onclick = () => {
+  fetch("/logout", { method:"POST" })
   .then(()=> location.href="/");
-});
+}
 
-sendBtn.onclick = ()=>{
+sendBtn.onclick = () => {
+  sendBtn.disabled = true;
+  sendBtn.innerText = "Sending...";
 
-  sendBtn.disabled=true;
-  sendBtn.innerText="Sending...";
-
-  fetch("/send",{
-    method:"POST",
-    headers:{ "Content-Type":"application/json" },
-    body:JSON.stringify({
-      senderName:senderName.value,
-      email:email.value.trim(),
-      password:pass.value.trim(),
-      subject:subject.value,
-      message:message.value,
-      recipients:recipients.value.trim()
+  fetch("/send", {
+    method: "POST",
+    headers: {"Content-Type":"application/json"},
+    body: JSON.stringify({
+      senderName: senderName.value,
+      email: email.value,
+      password: pass.value,
+      subject: subject.value,
+      message: message.value,
+      recipients: recipients.value
     })
   })
   .then(r=>r.json())
   .then(d=>{
-
-    if(d.success){
-      statusMessage.innerText = `Mail Sent Successfully ✅ (${d.sent})`;
-      alert(`Mail Sent Successfully ✅ (${d.sent})`);
-    }
-    else if(d.type==="wrongpass"){
-      statusMessage.innerText="Wrong App Password ❌";
-      alert("Wrong App Password ❌");
-    }
-    else if(d.type==="limit"){
-      statusMessage.innerText="Limit Finished ❌";
-      alert("Limit Finished ❌");
-    }
-    else{
-      statusMessage.innerText="Failed ❌";
-      alert("Failed ❌");
-    }
-
+    statusMessage.innerText = d.message;
+    alert(d.message);
   })
   .finally(()=>{
-    sendBtn.disabled=false;
-    sendBtn.innerText="Send All";
-  });
-};
+    sendBtn.disabled = false;
+    sendBtn.innerText = "Send All";
+  })
+}
