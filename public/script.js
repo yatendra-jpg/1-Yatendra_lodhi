2ffb6b3d81920f1e179df572e2a19d541954c2f2
yatendra-jpg <yatendra@yatendrabulkmailng.info>
2025-12-09 20:05:26 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,8 +1,14 @@
+logoutBtn.addEventListener("dblclick", () => {
+  fetch("/logout", { method:"POST" })
+    .then(()=> location.href="/");
+});
+
 sendBtn.onclick = () => {
+
   sendBtn.disabled = true;
   sendBtn.innerText = "Sending...";
 
-  fetch("/send", {
+  fetch("/send",{
     method:"POST",
     headers:{ "Content-Type":"application/json" },
     body:JSON.stringify({
@@ -17,27 +23,14 @@ sendBtn.onclick = () => {
   .then(r=>r.json())
   .then(d=>{
     if(d.success){
-      popupMsg.innerText = `Mail Sent Successfully (${d.sent})`;
-      openPopup();
+      statusMessage.innerText = `Sent: ${d.sent}`;
+      alert(`Sent Successfully (${d.sent})`);
     } else {
-      popupMsg.innerText = "Mail Not Sent";
-      openPopup();
+      alert("Limit reached OR Wrong App password");
     }
   })
   .finally(()=>{
-    sendBtn.disabled=false;
-    sendBtn.innerText="Send All";
+    sendBtn.disabled = false;
+    sendBtn.innerText = "Send All";
   });
 };
-
-popupBtn.onclick = () => closePopup();
-
-function openPopup(){
-  popupOverlay.style.display="block";
-  popupBox.style.display="block";
-}
-
-function closePopup(){
-  popupOverlay.style.display="none";
-  popupBox.style.display="none";
-}
