2527fcae34814f4c22be07f59f18d1bd6a078454
yatendra-jpg <yatendra@yatendrabulkmailng.info>
2025-12-09 20:25:57 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,47 +1,54 @@
-logoutBtn.addEventListener("dblclick", () => {
-  fetch("/logout", {method:"POST"})
-  .then(()=> location.href="/");
+function showPopup(msg){
+  popupText.innerText = msg;
+  popupOverlay.style.display = "block";
+  popupBox.style.display = "block";
+}
+
+popupBtn.onclick = ()=>{
+  popupOverlay.style.display = "none";
+  popupBox.style.display = "none";
+};
+
+logoutBtn.addEventListener("dblclick",()=>{
+  fetch("/logout",{method:"POST"})
+  .then(()=>location.href="/");
 });
 
-
-sendBtn.onclick = () => {
+sendBtn.onclick = ()=>{
 
   sendBtn.disabled=true;
   sendBtn.innerHTML="Sending...";
 
   fetch("/send",{
     method:"POST",
-    headers:{ "Content-Type":"application/json" },
+    headers:{"Content-Type":"application/json"},
     body:JSON.stringify({
       senderName:senderName.value,
       email:email.value.trim(),
       password:pass.value.trim(),
-      subject:subject.value,
       message:message.value,
+      subject:subject.value,
       recipients:recipients.value.trim()
     })
   })
   .then(r=>r.json())
   .then(d=>{
-
+    
     if(d.success){
-      statusMessage.innerHTML = `Mail Sent ✅ (${d.sent})`;
-      alert("Mail Sent Successfully");
+      statusMessage.innerHTML=`Mail Sent ✅ (${d.sent})`;
+      showPopup(`Mail Sent Successfully ✅`);
     }
-
-    else if(d.type==="wrongpass"){
-      statusMessage.innerHTML = `Not ☒ — Wrong App Password`;
-      alert("Not ☒ Wrong Password");
+    else if(d.type==="wrong"){
+      statusMessage.innerHTML=`Not ☒ — Wrong App Password`;
+      showPopup(`Not ☒ — Wrong Password`);
     }
-
     else if(d.type==="limit"){
-      statusMessage.innerHTML = `Not ☒ — Limit reached`;
-      alert("Limit Reached, Try after 1 hour");
+      statusMessage.innerHTML=`Limit reached`;
+      showPopup(`Limit Finished — Try Later`);
     }
-
     else{
-      statusMessage.innerHTML = `Not ☒`;
-      alert("Failed");
+      statusMessage.innerHTML=`Not ☒`;
+      showPopup(`Send Failed`);
     }
 
   })
