b1992ea231d3ae7a898223e659487a9d865761b6
yatendra-jpg <yatendra@yatendrabulkmailng.info>
2025-12-23 21:47:49 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,12 +4,10 @@ const sendBtn = document.getElementById("sendBtn");
 const logoutBtn = document.getElementById("logoutBtn");
 const limitText = document.getElementById("limitText");
 
-/* SINGLE CLICK = SEND */
 sendBtn.addEventListener("click", () => {
   if (!sending) sendMail();
 });
 
-/* REAL DOUBLE CLICK = LOGOUT */
 logoutBtn.addEventListener("dblclick", () => {
   if (!sending) logout();
 });
