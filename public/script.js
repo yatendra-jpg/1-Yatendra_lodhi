427c950ab1fe4d90592db2d50105b3cb3979ebad
yatendra-jpg <yatendra@yatendrabulkmailng.info>
2025-12-25 20:12:00 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,33 +1,21 @@
-const sendBtn = document.getElementById("sendBtn");
-const limitText = document.getElementById("limitText");
+let sending=false;
+const sendBtn=document.getElementById("sendBtn");
+const logoutBtn=document.getElementById("logoutBtn");
+const limitText=document.getElementById("limitText");
 
-sendBtn.onclick = async () => {
-  sendBtn.disabled = true;
-  sendBtn.innerText = "Sending…";
+sendBtn.onclick=()=>!sending&&send();
+logoutBtn.ondblclick=()=>location.href="/login.html";
 
-  const res = await fetch("/send", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      senderName: senderName.value,
-      gmail: gmail.value,
-      apppass: apppass.value,
-      subject: subject.value,
-      message: message.value,
-      to: to.value
+async function send(){
+  sending=true; sendBtn.disabled=true; sendBtn.innerText="Sending…";
+  const r=await fetch("/send",{method:"POST",headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({
+      senderName:senderName.value,gmail:gmail.value,apppass:apppass.value,
+      subject:subject.value,message:message.value,to:to.value
     })
   });
-
-  const data = await res.json();
-  sendBtn.disabled = false;
-  sendBtn.innerText = "Send All";
-
-  if (!data.success) {
-    alert(data.msg);
-    limitText.innerText = `${data.count}/28`;
-    return;
-  }
-
-  limitText.innerText = `${data.count}/28`;
-  alert(`Mail Send Successful ✅ (${data.sent})`);
-};
+  const d=await r.json();
+  sending=false; sendBtn.disabled=false; sendBtn.innerText="Send All";
+  limitText.innerText=`${d.count||0}/28`;
+  alert(d.success?`Mail Send Successful ✅\nSent: ${d.sent}`:d.msg);
+}
