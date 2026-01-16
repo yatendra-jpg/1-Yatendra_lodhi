b29fda23d5b6dd383ced2ea4e39a7e9cfd701d05
yatendra-jpg <yatendra@yatendrabulkmailng.info>
2026-01-16 07:32:08 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,38 @@
-let sending=false;
-sendBtn.onclick=()=>!sending&&sendMail();
-logoutBtn.ondblclick=()=>!sending&&(location.href="/login.html");
+let sending = false;
 
-async function sendMail(){
-  sending=true; sendBtn.disabled=true;
-  const res=await fetch("/send",{method:"POST",headers:{"Content-Type":"application/json"},
-    body:JSON.stringify({senderName:senderName.value,gmail:gmail.value,apppass:apppass.value,
-      subject:subject.value,message:message.value,to:to.value})});
-  const data=await res.json();
-  sending=false; sendBtn.disabled=false;
-  limitText.innerText=`${data.count}/28`;
-  if(!data.success) return alert(data.msg);
+sendBtn.addEventListener("click", () => {
+  if (!sending) sendMail();
+});
+
+logoutBtn.addEventListener("dblclick", () => {
+  if (!sending) location.href = "/login.html";
+});
+
+async function sendMail() {
+  sending = true;
+  sendBtn.disabled = true;
+  sendBtn.innerText = "Sending…";
+
+  const res = await fetch("/send", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      senderName: senderName.value,
+      gmail: gmail.value,
+      apppass: apppass.value,
+      subject: subject.value,
+      message: message.value,
+      to: to.value
+    })
+  });
+
+  const data = await res.json();
+
+  sending = false;
+  sendBtn.disabled = false;
+  sendBtn.innerText = "Send";
+
+  limitText.innerText = `${data.count}/28`;
+  if (!data.success) return alert(data.msg);
   alert(`Mail sent ✅ (${data.sent})`);
 }
